a1f5773a4c0a778b69e4fd4646a0f31ab064ce24
first commit <tharusanduni33@gmail.com>
2024-09-11 11:56:18 +0530

    config DBConnection

--- src/edu/ijse/mvc/db/DBConnection.java
+++ src/edu/ijse/mvc/db/DBConnection.java
@@ -4,10 +4,34 @@
  */
 package edu.ijse.mvc.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author tharu
  */
 public class DBConnection {
+    private static DBConnection dbconnection;
     
+    private Connection connection;
+    
+    private DBConnection() throws SQLException,ClassNotFoundException{
+        
+        Class.forName("com.mysql.cj.jdbo.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket","root","mysql");
+    }
+    
+    public static DBConnection getInstance() throws SQLException,ClassNotFoundException{
+        
+        if(dbconnection == null){
+        dbconnection = new DBConnection();
+        }
+        return dbconnection;
+    }
+    
+    public Connection getConnection(){
+        return connection;
+    }
+   
 }
