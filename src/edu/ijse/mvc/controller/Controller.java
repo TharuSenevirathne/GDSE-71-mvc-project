feb780c133d57d2f9b9c713ef4366cf50945d4fa
first commit <tharusanduni33@gmail.com>
2024-09-11 15:38:03 +0530

    config controller

--- src/edu/ijse/mvc/controller/Controller.java
+++ src/edu/ijse/mvc/controller/Controller.java
@@ -4,10 +4,38 @@
  */
 package edu.ijse.mvc.controller;
 
+import edu.ijse.mvc.model.model;
+import edu.ijse.mvc.dto.Dto;
+import java.util.List;
 /**
  *
  * @author tharu
  */
 public class Controller {
+    private model model = new model();
     
+    public String save (Dto Dto) throws Exception {
+    String resp = model.save(Dto);
+    return resp;
+    }
+    
+    public String update (Dto Dto) throws Exception {
+    String resp = model.update(Dto);
+    return resp;
+    }
+    
+    public String delete (String Id) throws Exception {
+    String resp = model.delete(Id);
+    return resp;
+    }
+    
+    public Dto search (String Id) throws Exception {
+    Dto dto = model.search(Id);
+    return dto;
+    }
+    public List< Dto > getAll () throws Exception {
+    List< Dto > Dtos = model.getAll();
+    return Dtos;
+    }
+  
 }
